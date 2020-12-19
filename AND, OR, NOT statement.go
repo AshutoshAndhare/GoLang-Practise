9077a11b3b3c785cf2527637429be8f3d024263c
AshutoshAndhare <62891731+AshutoshAndhare@users.noreply.github.com>
2020-12-19 23:35:47 +0530

    Conditional statements, Logical Operators

---
+++ AND, OR, NOT statement.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	rightTime := true
+	rightPlace := true
+
+	// Edit this condition for the FIRST checkpoint
+	if rightTime && rightPlace {
+		fmt.Println("We're outta here!")
+	} else {
+		fmt.Println("Be patient...")
+	}
+
+  
+	enoughRobbers := false
+	enoughBags := true
+
+	// Edit this condition for the SECOND checkpoint
+	if enoughRobbers || enoughBags{
+		fmt.Println("Grab everything!")
+	} else {
+		fmt.Println("Grab whatever you can!")
+	}
+
+	readyToGo := true
+
+	if readyToGo {
+		fmt.Println("Start the car!")
+	} else {
+		fmt.Println("What are we waiting for??")
+	}
+}
